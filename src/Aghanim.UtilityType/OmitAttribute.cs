1c559b96bc197d5780140911fac3cc7ceaf46ae0
bojack <1412393182@qq.com>
2025-05-09 13:25:06 +0800

    Init Repository

---
+++ src/Aghanim.UtilityType/OmitAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Aghanim.UtilityType;
+
+[AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+public class OmitAttribute<T> : Attribute
+{
+
+    public OmitAttribute(params string[] args)
+    {
+        _ = args;
+    }
+
+}
---
+++ src/Aghanim.UtilityType/PickAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace Aghanim.UtilityType;
+[AttributeUsage(AttributeTargets.Interface | AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = true)]
+public class PickAttribute<T> : Attribute 
+{
+
+    public PickAttribute(params string[] args)    
+    {
+        _ = args;
+    }
+
+}
+
