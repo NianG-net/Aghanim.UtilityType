eda34acac2489af42aedf1ad1d17e32bd20e80eb
bojack <1412393182@qq.com>
2025-05-12 11:56:18 +0800

    use GeneratorAttributeSyntaxContext  update code

--- src/Aghanim.UtilityType.Generator/UtilityTypeGenerator.cs
+++ src/Aghanim.UtilityType.Generator/UtilityTypeGenerator.cs
@@ -1,13 +1,11 @@
-﻿using Aghanim.UtilityType.Generator.Models;
-
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
-using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
@@ -15,44 +13,32 @@ namespace Aghanim.UtilityType.Generator;
 
 public abstract class UtilityTypeGenerator : IIncrementalGenerator
 {
-    public SyntaxKind ParseTypeKind(TypeKind typeKind, bool isRecord) => (typeKind, isRecord) switch
-    {
-        (TypeKind.Class, false) => SyntaxKind.ClassDeclaration,
-        (TypeKind.Class, true) => SyntaxKind.RecordDeclaration,
-        (TypeKind.Struct, false) => SyntaxKind.StructDeclaration,
-        (TypeKind.Struct, true) => SyntaxKind.RecordStructDeclaration,
-        (TypeKind.Interface, _) => SyntaxKind.InterfaceDeclaration,
-        _ => throw new ArgumentException("Invalid type kind", nameof(typeKind))
-    };
     public abstract IEnumerable<ISymbol> FilterSymbol(IEnumerable<ISymbol> symbols, ImmutableHashSet<string> constructorArguments);
-    public IEnumerable<(string hitName, string source)> ParseSource(UtilityTypeDeclaration declaration)
+    public IEnumerable<(string hitName, string source)> ParseSource(GeneratorAttributeSyntaxContext context)
     {
-        var containingNamespace = declaration.TargetSymbol.ContainingNamespace;
-        var fileUsings = containingNamespace.DeclaringSyntaxReferences.SelectMany(x => x.SyntaxTree.GetRoot().DescendantNodes().OfType<UsingDirectiveSyntax>());
-        var namespaceUsing = containingNamespace.DeclaringSyntaxReferences.SelectMany(x => x.GetSyntax().ChildNodes().OfType<UsingDirectiveSyntax>());
-        var targetSymbol = declaration.TargetSymbol;
+        var values =
 
-
-        var attributeDict =
-
-        from attr in declaration.Attributes
+        from attr in context.Attributes
         let TypeSymbol = attr.AttributeClass.TypeArguments[0]
         let ConstructorArguments = from typedConstant in attr.ConstructorArguments
                                    from innerTypedConstant in typedConstant.Values
                                    select innerTypedConstant.Value.ToString()
-        select new { TypeSymbol, ConstructorArguments } into temp
-        group temp by temp.TypeSymbol.ToDisplayString(SymbolDisplayFormat.FullyQualifiedFormat) into g
-        let TypeSymbol = g.First().TypeSymbol
-        select new { TypeSymbol, PropertySymbols = TypeSymbol.GetMembers().OfType<IPropertySymbol>(), ConstructorArguments = g.SelectMany(x => x.ConstructorArguments).ToImmutableHashSet() };
+        let PropertySymbols = TypeSymbol.GetMembers().OfType<IPropertySymbol>()
+        select new { PropertySymbols, TypeSymbol, ConstructorArguments };
+
+        var attributeDict = values.ToDictionary(x => x.TypeSymbol, v => v, SymbolEqualityComparer.Default);
+        //into temp
+        //group temp by temp.TypeSymbol into g
+        //let TypeSymbol = g.First().TypeSymbol
+        //select new { TypeSymbol, PropertySymbols = TypeSymbol.GetMembers().OfType<IPropertySymbol>(), ConstructorArguments = g.SelectMany(x => x.ConstructorArguments).ToImmutableHashSet() };
 
 
 
-
-        foreach (var attributePair in attributeDict)
+        foreach (var attributePair in attributeDict.Values)
         {
             HashSet<MemberDeclarationSyntax> propertys = [];
 
-            var symbols = FilterSymbol(attributePair.PropertySymbols, attributePair.ConstructorArguments);
+            var symbols = FilterSymbol(attributePair.PropertySymbols, attributePair.ConstructorArguments.ToImmutableHashSet());
 
             foreach (ISymbol propertySymbol in symbols)
             {
@@ -69,30 +55,15 @@ public abstract class UtilityTypeGenerator : IIncrementalGenerator
             }
 
 
-            string hintName = $"{targetSymbol.ToDisplayString()}.{attributePair.TypeSymbol.ToDisplayString()}.g.cs";
+            string hintName = $"{context.TargetSymbol.ToDisplayString()}.{attributePair.TypeSymbol.ToDisplayString()}.g.cs";
 
 
-            SyntaxKind typeKind = ParseTypeKind(targetSymbol.TypeKind, targetSymbol.IsRecord);
-            var typeDeclaration = TypeDeclaration(typeKind, targetSymbol.Name)
-                .WithModifiers(TokenList(
-                    Token(SyntaxKind.PartialKeyword)
-                    .WithLeadingTrivia(ParseLeadingTrivia("#nullable enable \r #pragma warning disable CS8618 "))
-                    ))
+
+            var typeDeclaration = Unsafe.As<TypeDeclarationSyntax>(context.TargetNode)
                 .WithMembers(List(propertys));
+            var compilationUnit = context.SemanticModel.SyntaxTree.GetCompilationUnitRoot()
+                                                                  .WithMembers(SingletonList<MemberDeclarationSyntax>(typeDeclaration));
 
-            var compilationUnit = CompilationUnit().WithUsings(List(fileUsings)); ;
-            if (containingNamespace.IsGlobalNamespace)
-            {
-                compilationUnit = compilationUnit
-                    .WithMembers(SingletonList<MemberDeclarationSyntax>(typeDeclaration));
-            }
-            else
-            {
-                compilationUnit = compilationUnit
-                    .WithMembers(SingletonList<MemberDeclarationSyntax>(NamespaceDeclaration(ParseName(containingNamespace.ToDisplayString()))
-                    .WithUsings(List(namespaceUsing))
-                    .WithMembers(SingletonList<MemberDeclarationSyntax>(typeDeclaration))));
-            }
 
 
             yield return (hintName, compilationUnit.NormalizeWhitespace().ToFullString());
@@ -108,10 +79,9 @@ public abstract class UtilityTypeGenerator : IIncrementalGenerator
         var declaration = context.SyntaxProvider.ForAttributeWithMetadataName(
             AttributeFullName,
             static (s, _) => s is TypeDeclarationSyntax tds && tds.Modifiers.Any(SyntaxKind.PartialKeyword),
-            static (ctx, _) => (ctx.Attributes, ctx.TargetSymbol)
-            )
+            static (ctx, _) => ctx)
             .Where(x => x.TargetSymbol is INamedTypeSymbol)
-            .Select((x, _) => new UtilityTypeDeclaration(x.Attributes, (INamedTypeSymbol)x.TargetSymbol));
+            .Select((x, _) => x);
 
         context.RegisterSourceOutput(declaration, (sourceContext, utilityTypeDeclaration) =>
         {
